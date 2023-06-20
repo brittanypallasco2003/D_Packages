1ed851dfdacf98aacc54750e7feb457f0e14c4af
brittanypallasco2003 <nohemiespinel0@gmail.com>
2023-06-20 01:04:48 -0500

    se crean las instancias de las clases del equipo contrario

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
 import Conicas2.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Instancias de la clase circunferencia_grupo2
+        circunferencia_grupo2 fig1=new circunferencia_grupo2(2.345f,5.45f);
+        circunferencia_grupo2 fig2 = new circunferencia_grupo2(6.45f,8.96f);
+
+        //Instancias de la clase Elipse
+        Elipse fig3 = new Elipse(4.878, 96.723);
+        Elipse fig4 = new Elipse(9.785, 15.657);
+
+        //Instancias de la clase hiperbole
+        hiperbole fig5 = new hiperbole(7.1278,5.5656);
+        hiperbole fig6 = new hiperbole(12.4568, 16.4784);
+
+        //Instancias de la clase Parabola_Equipo2
+        Parabola_Equipo2 fig7 = new Parabola_Equipo2(23.48f,32.48f, 14.98f,16.45f,12.65f);
+        Parabola_Equipo2 fig8 = new Parabola_Equipo2(12.47f, 23.14f, 25.45f, 56.12f, 32.14f);
+
     }
 //
 }
